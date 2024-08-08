eba626fdae7094321bb25ef47094b9dfa6fde476
NooisY <143975472+lN00ISYl@users.noreply.github.com>
2024-08-08 22:00:49 +0800

    first

--- app/src/main/java/com/example/testttttt/MainActivity.kt
+++ app/src/main/java/com/example/testttttt/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.testttttt
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val test = findViewById<TextView>(R.id.textView)
     }
 }
